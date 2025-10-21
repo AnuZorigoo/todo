a7426ddfa70ad47f4f187ed259dd1b76342fa713
25LP7596 25LP7596 <25LP7596@admins-iMac.local>
2025-10-21 12:30:25 +0800

    change 1021

--- index.js
+++ index.js
@@ -1,42 +1,76 @@
 const list = document.querySelector(".list");
 const input = document.querySelector(".todo");
-const button = document.querySelector(".addButn");
+const addBtn = document.querySelector(".add-btn");
+const filterButtons = document.querySelectorAll(".filter-btn");
 
 let content = [];
+let type = "All";
 
-const ListItem = (content, isChecked) => {
+let id = 1;
+
+const ListItem = (item) => {
   return `
      <div class="item">
-          <input type="checkbox" id="myCheckbox"/>
-          <p>${content}</p>
+          <input type="checkbox" id="Checkbox" ${item.isDone ? "checked" : ""}/>
+          <p>${item.text}</p>
           <button class="delete-btn">Delete</button>
         </div>
     `;
 };
+addBtn.addEventListener("click", () => {
+  content.push({
+    id: id,
+    text: input.value,
+    isDone: false,
+  });
+  id++;
+
+  render();
+});
+
+filterButtons.forEach((btn, i) => {
+  btn.addEventListener("click", () => {
+    filterButtons.forEach((button) => {
+      button.classList.remove("chosen");
+    });
+    btn.classList.add("chosen");
+    if (i == 0) {
+      type = "All";
+    } else if (i == 1) {
+      type = "Active";
+    } else if (i == 2) {
+      type = "Completed";
+    }
+    render();
+  });
+});
 const render = () => {
-  list.innerHTML = content
-    .map((item) => {
-      return ListItem(item);
+  const elements = content
+    .filter((item) => {
+      if (type === "All") return true;
+      if (type === "Active") return item.isDone === false;
+      return item.isDone === true;
     })
+    .map((item) => ListItem(item))
     .join("");
+  list.innerHTML = elements;
   addListener();
 };
 
 const addListener = () => {
   const deleteBtns = document.querySelectorAll(".delete-btn");
 
-  deleteBtns.forEach((btn, i) => {
+  deleteBtns.forEach((btn) => {
     btn.addEventListener("click", () => {
       content = content.filter((item, index) => index !== i);
       render();
     });
   });
 };
-
-button.addEventListener("click", () => {
-  const value = input.value;
-  content.push(value);
-
-  render();
+const checkboxes = document.querySelectorAll(".checkbox");
+checkboxes.forEach((checkbox, i) => {
+  checkbox.addEventListener("click", () => {
+    content[i].isDone = !content[i].isDone;
+    render();
+  });
 });
-let completedTask = [];
